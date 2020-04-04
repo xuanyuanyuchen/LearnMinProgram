c0bc9f7c2dfa0cf75137cd552b6d2b33f77b68d6
张鹏 <5659588+xuanyuanyuchen@user.noreply.gitee.com>
2020-04-04 21:44:45 +0800

    注册小程序和页面

---
+++ app.js
@@ -0,0 +1,37 @@
+//注册一个小程序示例
+App({
+  //小程序初始化完成时
+  onLaunch: function (options) {
+    //获取用户信息
+    console.log(options)
+  },
+  //小程序显示出来时
+  onShow: function (options) {
+    //1.判断小程序的进入场景
+    console.log(options)
+    switch(options.scene){
+      case 1001:
+        break;
+      case 1005:
+        break;
+    }
+    //2.获取用户的信息，并且获取到用户信息之后，将用户的信息传递给服务器
+    wx.getUserInfo({//在onShow每次进入都会执行，而在onLaunch只执行一次，并且已废弃
+      success: function(res) {
+        console.log(res)
+      }
+    })
+  },
+  //小程序隐藏时
+  onHide: function () {
+    
+  },
+  //小程序产生一些错误
+  onError: function (msg) {
+    
+  },
+  globalData: {//在这里面定义的数据可以共享
+    name: 'coderwhy',
+    age: 18
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,68 @@
 // pages/home/home.js
+//getApp()获取App()产生的示例对象
+// const app = getApp()
+// console.log(app.globalData.name)
+// console.log(app.globalData.age)
+// const name = app.globalData.name
+// const age = app.globalData.age
+
+//注册一个页面
+//页面也有自己的生命周期函数
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // 2.初始化数据
   data: {
-
+    message: '哈哈哈',
+    list: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 1.监听页面的生命周期函数
+  //页面被加载出来
+  onLoad() {
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      //箭头函数中的this一层层向上找
+      success: (res) => {
+        console.log(res)
+        const data = res.data.data.list
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //页面显示出来时
+  onReady() {
+    console.log('onReady')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //页面初次渲染完成时
+  onShow() {
+    console.log('onShow')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //当页面隐藏起来时
+  onHide() {
+    console.log('onHide')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onUnload() {
+    console.log('onUnload')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // 3.监听wxml中相关的一些事件
+  handleGetUserInfo(event) {
+    console.log(event)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleViewClick() {
+    console.log('哈哈哈被点击了')
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 4.其他事件
+  //监听页面的滚动
+  onPageScroll(obj) {
+    console.log(obj)
+  },
+  //监听页面滚动到低部
+  onReachBottom() {
+    console.log('页面滚动到底部')
+  },
+  onPullDownRefresh() {
+    console.log('下拉刷新的事件')
   }
 })
