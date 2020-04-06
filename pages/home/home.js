bda2110ffff6fa902e51cb63a425096bc68cdf60
张鹏 <5659588+xuanyuanyuchen@user.noreply.gitee.com>
2020-04-06 19:58:11 +0800

    知识点12

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    counter: 0
+    counter: 0,
+    isShow: true
   },
   handleIncrement(event) {
     console.log('------',event)
@@ -15,5 +16,24 @@ Page({
   },
   handleTabClick(event) {
     console.log(event)
+  },
+  handleIncrementCpn() {
+    //最终目的：修改my-sel中的counter
+    //1.获取组件对象
+    const my_sel = this.selectComponent('.sel-class')
+    console.log(my_sel)
+
+    //2.通过setData修改组件中的数据(不合适)
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 20
+    // })
+
+    //3.通过方法对数据进行修改
+    my_sel.incrementCounter(10)
+  },
+  handleChangeShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
   }
 })
