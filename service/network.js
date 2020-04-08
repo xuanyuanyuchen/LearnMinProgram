baf77b748366086dafdebf2bec1b3d131f7ef555
张鹏 <5659588+xuanyuanyuchen@user.noreply.gitee.com>
2020-04-08 16:08:47 +0800

    知识点13

---
+++ service/network.js
@@ -0,0 +1,26 @@
+export default function request(options) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: options.url,
+      method: options.method || 'get',
+      data: options.data || {},
+      success: resolve,
+      fall: reject
+    })
+  })
+}
+// export default function request(options) {
+//   return new Promise((resolve, reject) => {
+//     wx.request({
+//       url: options.url,
+//       method: options.method || 'get',
+//       data: options.data || {},
+//       success: function(res) {
+//         resolve(res)
+//       },
+//       fall: function(err) {
+//         reject(err)
+//       }
+//     })
+//   })
+// }
